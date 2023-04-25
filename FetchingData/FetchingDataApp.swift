6c4bfe45a62394937decd90b241e7d50ee684faf
AndresMadeDev <andres.made.dev@gmail.com>
2023-04-25 10:30:02 -0400

    Initial Commit

---
+++ FetchingData/FetchingDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchingDataApp.swift
+//  FetchingData
+//
+//  Created by Andres Made on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchingDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
